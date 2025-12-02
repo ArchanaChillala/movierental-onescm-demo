5bd89b753b56178c8469d36f06c6695d5d33fda1
Archana Chillala <archanac@Archanas-MacBook-Pro.local>
2025-12-02 17:21:54 +0530

    Further modularize ChildrensMovie with better naming

--- src/main/java/movierental/ChildrensMovie.java
+++ src/main/java/movierental/ChildrensMovie.java
@@ -4,7 +4,7 @@ public class ChildrensMovie extends Movie {
     
     private static final double BASE_CHARGE = 1.5;
     private static final int THRESHOLD_DAYS = 3;
-    private static final double ADDITIONAL_CHARGE_PER_DAY = 1.5;
+    private static final double EXTRA_CHARGE_PER_DAY = 1.5;
     
     public ChildrensMovie(String title) {
         super(title);
@@ -22,7 +22,11 @@ public class ChildrensMovie extends Movie {
     }
 
     private static double extraCharge(int daysRented) {
-        return (daysRented - THRESHOLD_DAYS) * ADDITIONAL_CHARGE_PER_DAY;
+        return extra(daysRented) * EXTRA_CHARGE_PER_DAY;
+    }
+
+    private static int extra(int daysRented) {
+        return daysRented - THRESHOLD_DAYS;
     }
 
     private static boolean exceedsThreshold(int daysRented) {
